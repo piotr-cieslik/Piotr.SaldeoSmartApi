5399709014a0304434f0f7efec6f55791039dd71
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 17:12:54 +0200

    Add extension methods that simplify serialization and deserialization

---
+++ Piotr.SaldeoSmartApi/Serialization/HttpResponseMessageExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Piotr.SaldeoSmartApi.Serialization
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<DataStructures.Response> Deserialize(this Task<HttpResponseMessage> response)
+        {
+            var content = await (await response).Content.ReadAsStringAsync();
+            var serializer = new XmlSerializer();
+            return serializer.Deserialize(content);
+        }
+    }
+}
---
+++ Piotr.SaldeoSmartApi/Serialization/RootExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Piotr.SaldeoSmartApi.Serialization
+{
+    public static class RootExtensions
+    {
+        /// <summary>
+        /// Helper method that performs serialization, compression and conversion to base64.
+        /// </summary>
+        /// <param name="root">Root element of the request.</param>
+        /// <returns>Base64 string ready to send as command parameter.</returns>
+        public static Base64String Serialize(this DataStructures.Root root)
+        {
+            var serializer = new XmlSerializer();
+            var xml = serializer.Serialize(root);
+            var gZiped = new GZippedString(xml);
+            var base64 = new Base64String(gZiped);
+            return base64;
+        }
+    }
+}
