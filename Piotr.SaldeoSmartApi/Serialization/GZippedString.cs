8540061ffebff30fddfb020e9954de3b57bfb2f6
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 09:27:11 +0200

    Add type GZippedString

---
+++ Piotr.SaldeoSmartApi/Serialization/GZippedString.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Piotr.SaldeoSmartApi.Serialization
+{
+    /// <summary>
+    /// Wrapper that takes string as input parameter (by constructor),
+    /// performs gZip compression and return compressed string as array of bytes. 
+    /// </summary>
+    public sealed class GZippedString
+    {
+        private readonly string _value;
+
+        public GZippedString(string value) => _value = value;
+
+        public static implicit operator byte[](GZippedString x) => x.Bytes();
+
+        public byte[] Bytes()
+        {
+            using (var sourceStream = new MemoryStream(Encoding.UTF8.GetBytes(_value)))
+            {
+                using (var targetStream = new MemoryStream())
+                {
+                    using (var gZipStream = new GZipStream(targetStream, CompressionLevel.Optimal))
+                    {
+                        sourceStream.CopyTo(gZipStream);
+                    }
+                    return targetStream.ToArray();
+                }
+            }
+        }
+    }
+}
