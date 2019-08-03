c461619bc24abbccd54c9e256b59bed6c1b77ad9
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 09:26:50 +0200

    Add type Base64String

---
+++ Piotr.SaldeoSmartApi/Serialization/Base64String.cs
@@ -0,0 +1,13 @@
+﻿namespace Piotr.SaldeoSmartApi.Serialization
+{
+    public sealed class Base64String
+    {
+        private readonly byte[] _value;
+
+        public Base64String(byte[] value) => _value = value;
+
+        public static implicit operator string(Base64String x) => x.ToString();
+
+        public override string ToString() => System.Convert.ToBase64String(_value);
+    }
+}
