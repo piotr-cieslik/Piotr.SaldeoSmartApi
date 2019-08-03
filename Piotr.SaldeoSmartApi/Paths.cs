1a12331f12fc6a9e59456868275268f88c2a9e47
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 10:14:08 +0200

    Add type Paths

---
+++ Piotr.SaldeoSmartApi/Paths.cs
@@ -0,0 +1,175 @@
+﻿namespace Piotr.SaldeoSmartApi
+{
+    public static class Paths
+    {
+        /// <summary>
+        /// SS01 company.create
+        /// </summary>
+        public static class CompanyCreate
+        {
+            public const string Version0 = "/api/xml/1.0/company/create";
+        }
+
+        /// <summary>
+        /// SS02 contractor.merge
+        /// </summary>
+        public static class ContractorMerge
+        {
+            public const string Version0 = "/api/xml/1.0/contractor/merge";
+        }
+
+        /// <summary>
+        /// SS03 document.list
+        /// </summary>
+        public static class DocumentList
+        {
+            public const string Version0 = "/api/xml/1.0/document/list";
+
+            public const string Version21 = "/api/xml/1.21/document/list";
+        }
+
+        /// <summary>
+        /// SS04 company.list
+        /// </summary>
+        public static class CompanyList
+        {
+            public const string Version0 = "/api/xml/1.0/company/list";
+        }
+
+        /// <summary>
+        /// SS05 document.add
+        /// </summary>
+        public static class DocumentAdd
+        {
+            public const string Version0 = "/api/xml/1.0/document/add";
+        }
+
+        /// <summary>
+        /// SS06 document.recognize
+        /// </summary>
+        public static class DocumentRecognize
+        {
+            public const string Version0 = "/api/xml/1.0/document/recognize";
+        }
+
+        /// <summary>
+        /// SS07 contractor.list
+        /// </summary>
+        public static class ContractorList
+        {
+            public const string Version0 = "/api/xml/1.0/contractor/list";
+        }
+
+        /// <summary>
+        /// SS08 document.list_recognized
+        /// </summary>
+        public static class DocumentListRecognized
+        {
+            public const string Version0 = "/api/xml/1.0/document/list_recognized";
+        }
+
+        /// <summary>
+        /// SS09 category.merge
+        /// </summary>
+        public static class CategoryMerge
+        {
+            public const string Version0 = "/api/xml/1.0/category/merge";
+        }
+
+        /// <summary>
+        /// SS10 register.merge
+        /// </summary>
+        public static class RegisterMerge
+        {
+            public const string Version0 = "/api/xml/1.0/register/merge";
+        }
+
+        /// <summary>
+        /// SS11 payment_method.merge
+        /// </summary>
+        public static class PaymentMethodMerge
+        {
+            public const string Version0 = "/api/xml/1.0/payment_method/merge";
+        }
+
+        /// <summary>
+        /// SS12 dimension.merge
+        /// </summary>
+        public static class DimensionMerge
+        {
+            public const string Version0 = "/api/xml/1.0/dimension/merge";
+        }
+
+        /// <summary>
+        /// SS13 document.sync
+        /// </summary>
+        public static class DocumentSync
+        {
+            public const string Version0 = "/api/xml/1.0/document/sync";
+        }
+
+        /// <summary>
+        /// SS14 description.merge
+        /// </summary>
+        public static class DescriptionMerge
+        {
+            public const string Version0 = "/api/xml/1.0/description/merge";
+        }
+
+        /// <summary>
+        /// SS15 company.synchronize
+        /// </summary>
+        public static class CompanySynchronize
+        {
+            public const string Version0 = "/api/xml/1.0/company/synchronize";
+        }
+
+        /// <summary>
+        /// SS16 document.delete
+        /// </summary>
+        public static class DocumentDelete
+        {
+            public const string Version3 = "/api/xml/1.3/document/delete";
+        }
+
+        /// <summary>
+        /// SS17 document.update
+        /// </summary>
+        public static class DocumentUpdate
+        {
+            public const string Version3 = "/api/xml/1.3/document/update";
+        }
+
+        /// <summary>
+        /// SS18 document.search
+        /// </summary>
+        public static class DocumentSearch
+        {
+            public const string Version2 = "/api/xml/1.2/document/search";
+        }
+
+        /// <summary>
+        /// SS19 bank_statement.list
+        /// </summary>
+        public static class BankStatementList
+        {
+            public const string Version9 = "/api/xml/1.9/bank_statement/list";
+        }
+
+        /// <summary>
+        /// SS20 document_dimension.merge
+        /// </summary>
+        public static class DocumentDimensionMerge
+        {
+            public const string Version0 = "/api/xml/1.0/document_dimension/merge";
+        }
+
+        /// <summary>
+        /// SS21 article.merge
+        /// </summary>
+        public static class ArticleMerge
+        {
+            public const string Version14 = " /api/xml/1.14/article/merge";
+        }
+    }
+}
