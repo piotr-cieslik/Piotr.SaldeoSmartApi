6593944cee8f8f2695828b0eda15650cce3b7a7c
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 09:28:14 +0200

    Add type XmlSerializer

---
+++ Piotr.SaldeoSmartApi/Serialization/Utf8StringWriter.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Piotr.SaldeoSmartApi.Serialization
+{
+    internal sealed class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding => Encoding.UTF8;
+    }
+}
---
+++ Piotr.SaldeoSmartApi/Serialization/XmlSerializer.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace Piotr.SaldeoSmartApi.Serialization
+{
+    public sealed class XmlSerializer
+    {
+        public string Serialize(DataStructures.Root root)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(DataStructures.Root));
+            using (var stringWriter = new Utf8StringWriter())
+            {
+                serializer.Serialize(stringWriter, root);
+                var xml = stringWriter.ToString();
+                return xml;
+            }
+        }
+
+        public DataStructures.Response Deserialize(string xml)
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(DataStructures.Response));
+            using (var reader = new StringReader(xml))
+            {
+                var result = (DataStructures.Response)serializer.Deserialize(reader);
+                return result;
+            }
+        }
+    }
+}
