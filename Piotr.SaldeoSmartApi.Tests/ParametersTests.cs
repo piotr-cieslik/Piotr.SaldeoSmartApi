1a2d76593b478b5339511dd4b72997a668e2d987
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 10:11:14 +0200

    Add type Parameters

---
+++ Piotr.SaldeoSmartApi.Tests/ParametersTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace Piotr.SaldeoSmartApi.Tests
+{
+    public sealed class ParametersTests
+    {
+        [Fact]
+        public void ShouldBeImmutable()
+        {
+            var parameters1 = new Parameters();
+            var parameters2 = parameters1.AddUsername("pc");
+
+            Assert.Empty(parameters1);
+            Assert.Single(parameters2);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenAddingSecondParameterWithSameKey()
+        {
+            var parameters =
+                new Parameters()
+                    .AddUsername("a");
+            Assert.Throws<ArgumentException>(() => parameters.AddUsername("b"));
+        }
+    }
+}
---
+++ Piotr.SaldeoSmartApi/Parameters.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Piotr.SaldeoSmartApi
+{
+    public sealed class Parameters
+        : IEnumerable<KeyValuePair<string, string>>
+    {
+        private ImmutableDictionary<string, string> _dictionary;
+
+        public Parameters()
+        {
+            _dictionary = new Dictionary<string, string>().ToImmutableDictionary();
+        }
+
+        public Parameters(ImmutableDictionary<string, string> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+
+        public Parameters Add(string key, string value)
+        {
+            return new Parameters(_dictionary.Add(key, value));
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            return _dictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
