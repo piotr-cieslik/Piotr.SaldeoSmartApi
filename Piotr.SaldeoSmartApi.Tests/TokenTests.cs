20f42284c6109f51cae55d5415fa1d85eac641c1
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-03 10:12:56 +0200

    Add type Token

---
+++ Piotr.SaldeoSmartApi.Tests/TokenTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace Piotr.SaldeoSmartApi.Tests
+{
+    public sealed class TokenTests
+    {
+        [Fact]
+        public void ShouldCreate()
+        {
+            new Token("1");
+        }
+
+        [Theory]
+        [InlineData(default(string))]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void ShouldNotCreate(string value)
+        {
+            Assert.Throws<ArgumentException>(() => new Token(value));
+        }
+    }
+}
---
+++ Piotr.SaldeoSmartApi/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Piotr.SaldeoSmartApi
+{
+    public sealed class Token
+    {
+        private readonly string _value;
+
+        public Token(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Invalid token value.");
+            }
+
+            _value = value;
+        }
+
+        public static implicit operator string(Token x) => x.ToString();
+
+        public override string ToString() => _value;
+    }
+}
