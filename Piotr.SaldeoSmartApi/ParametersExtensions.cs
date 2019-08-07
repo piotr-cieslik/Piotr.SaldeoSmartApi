80e0a53fa156a87db5991919d7af8b5afeae978a
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-07 21:54:27 +0200

    Set extension methods in alphabetical order

--- Piotr.SaldeoSmartApi/ParametersExtensions.cs
+++ Piotr.SaldeoSmartApi/ParametersExtensions.cs
@@ -4,18 +4,18 @@ namespace Piotr.SaldeoSmartApi
 {
     public static class ParametersExtensions
     {
-        public static Parameters AddRequestIdBasedOnUtcTime(this Parameters parameters) => parameters.AddRequestId(DateTime.UtcNow.Ticks.ToString());
-
-        public static Parameters AddUsername(this Parameters parameters, string value) => parameters.Add("username", value);
-
-        public static Parameters AddRequestId(this Parameters parameters, string value) => parameters.Add("req_id", value);
-
-        public static Parameters AddRequestSignature(this Parameters parameters, string value) => parameters.Add("req_sig", value);
-
         public static Parameters AddCommand(this Parameters parameters, string value) => parameters.Add("command", value);
 
         public static Parameters AddCompanyProgramId(this Parameters parameters, string value) => parameters.Add("company_program_id", value);
 
         public static Parameters AddPolicy(this Parameters parameters, string value) => parameters.Add("policy", value);
+
+        public static Parameters AddRequestId(this Parameters parameters, string value) => parameters.Add("req_id", value);
+
+        public static Parameters AddRequestIdBasedOnUtcTime(this Parameters parameters) => parameters.AddRequestId(DateTime.UtcNow.Ticks.ToString());
+
+        public static Parameters AddRequestSignature(this Parameters parameters, string value) => parameters.Add("req_sig", value);
+
+        public static Parameters AddUsername(this Parameters parameters, string value) => parameters.Add("username", value);
     }
 }
