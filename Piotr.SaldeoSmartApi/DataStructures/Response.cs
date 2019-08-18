c78c44810fddc9a7392bffb6911661de36b3387a
Piotr Cieślik <piotr.cieslik@outlook.com>
2019-08-18 11:57:19 +0200

    Make results an array of objects, remove Results type

--- Piotr.SaldeoSmartApi/DataStructures/Response.cs
+++ Piotr.SaldeoSmartApi/DataStructures/Response.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Serialization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
 
 namespace Piotr.SaldeoSmartApi.DataStructures
 {
@@ -37,13 +39,19 @@ namespace Piotr.SaldeoSmartApi.DataStructures
         [XmlArrayItem("DOCUMENT_DELETE")]
         public DocumentDelete[] DocumentDeletes { get; set; }
 
-        // TODO Handle collection of results.
-        // Results may contain more than one element.
-        // The type of elements is dependent on performed operation.
-        // Introduce collections of different results for each operation.
-        // I suggest to name properties like "ResultsOfTypeDocument", "ResultsOfTypeCategory".
-        [XmlElement("RESULTS")]
-        public Results Results { get; set; }
+        [XmlArray("RESULTS")]
+        [XmlArrayItem(ElementName = "ARTICLE", Type = typeof(Article))]
+        [XmlArrayItem(ElementName = "CATEGORY", Type = typeof(Category))]
+        [XmlArrayItem(ElementName = "COMPANY", Type = typeof(Company))]
+        [XmlArrayItem(ElementName = "CONTRACTOR", Type = typeof(Contractor))]
+        [XmlArrayItem(ElementName = "DESCRIPTION", Type = typeof(Description))]
+        [XmlArrayItem(ElementName = "DIMENSION", Type = typeof(Dimension))]
+        [XmlArrayItem(ElementName = "DOCUMENT", Type = typeof(Document))]
+        [XmlArrayItem(ElementName = "DOCUMENT_DIMENSION", Type = typeof(DocumentDimension))]
+        [XmlArrayItem(ElementName = "DOCUMENTS_SYNCED", Type = typeof(DocumentsSynced))]
+        [XmlArrayItem(ElementName = "PAYMENT_METHOD", Type = typeof(PaymentMethod))]
+        [XmlArrayItem(ElementName = "REGISTER", Type = typeof(Register))]
+        public object[] Results { get; set; }
 
         [XmlArray("ARTICLES")]
         [XmlArrayItem("ARTICLE")]
@@ -55,5 +63,29 @@ namespace Piotr.SaldeoSmartApi.DataStructures
         [XmlArray("BANK_STATEMENTS")]
         [XmlArrayItem("BANK_STATEMENT")]
         public BankStatement[] BankStatements { get; set; }
+
+        public IEnumerable<T> ResultsOfType<T>() => Results?.Where(x => x.GetType() == typeof(T)).Cast<T>() ?? Enumerable.Empty<T>();
+
+        public IEnumerable<Article> ResultsOfTypeArticle() => ResultsOfType<Article>();
+
+        public IEnumerable<Category> ResultsOfTypeCategory() => ResultsOfType<Category>();
+
+        public IEnumerable<Company> ResultsOfTypeCompany() => ResultsOfType<Company>();
+
+        public IEnumerable<Contractor> ResultsOfTypeContractor() => ResultsOfType<Contractor>();
+
+        public IEnumerable<Description> ResultsOfTypeDescription() => ResultsOfType<Description>();
+
+        public IEnumerable<Dimension> ResultsOfTypeDimension() => ResultsOfType<Dimension>();
+
+        public IEnumerable<Document> ResultsOfTypeDocument() => ResultsOfType<Document>();
+
+        public IEnumerable<DocumentDimension> ResultsOfTypeDocumentDimension() => ResultsOfType<DocumentDimension>();
+
+        public IEnumerable<DocumentsSynced> ResultsOfTypeDocumentsSynced() => ResultsOfType<DocumentsSynced>();
+
+        public IEnumerable<PaymentMethod> ResultsOfTypePaymentMethod() => ResultsOfType<PaymentMethod>();
+
+        public IEnumerable<Register> ResultsOfTypeRegister() => ResultsOfType<Register>();
     }
 }
