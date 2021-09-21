fc3b8aeadf2d47d3dbe6e16001a66965d9ec31cb
Piotr Cieślik <piotr.cieslik@outlook.com>
2021-09-21 14:10:31 +0200

    Add example for document/list_recognized operation

---
+++ Piotr.SaldeoSmartApi.Examples/DocumentListRecognizedVersion21.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Piotr.SaldeoSmartApi.DataStructures;
+using Piotr.SaldeoSmartApi.Serialization;
+
+namespace Piotr.SaldeoSmartApi.Examples
+{
+    internal sealed class DocumentListRecognizedVersion21
+    {
+        public async Task Run()
+        {
+            // Specify your username.
+            var username = Environment.GetEnvironmentVariable("SaldeoSmartUsername");
+
+            // Specify your token.
+            var token = Environment.GetEnvironmentVariable("SaldeoSmartToken");
+
+            // Specify your company program id.
+            var companyProgramId = Environment.GetEnvironmentVariable("SaldeoSmartCompanyProgramId");
+
+            // Specify the server address.
+            var server = new Uri("https://saldeo.brainshare.pl");
+
+            // Create an instance of the API.
+            var api = new Api(server);
+
+            // Select path (operation).
+            var path = Paths.DocumentListRecognized.Version21;
+
+            // Specify document ids.
+            var request =
+                new Request
+                {
+                    OcrIdList = new string[]
+                    {
+                        "1",
+                    }
+                };
+
+            var command = request.Serialize();
+            var parameters =
+                new Parameters()
+                    .AddUsername(username)
+                    .AddRequestIdBasedOnUtcTime()
+                    .AddCommand(command);       
+            var httpRequest =
+                api.PostAsync(
+                    path,
+                    parameters,
+                    new Token(token));
+            var result =
+                await httpRequest.Deserialize();
+            Console.WriteLine(
+                string.Join(
+                    Environment.NewLine,
+                    result.Companies.Select(x => x.FullName)));
+        }
+    }
+}
--- Piotr.SaldeoSmartApi.Examples/Program.cs
+++ Piotr.SaldeoSmartApi.Examples/Program.cs
@@ -6,6 +6,7 @@ namespace Piotr.SaldeoSmartApi.Examples
     {
         static async Task Main(string[] args)
         {
+            await new DocumentListRecognizedVersion21().Run();
             await new CompanyListVersion0().Run();
             await new DocumentDimensionMergeVersion0().Run();
             await new DocumentListVersion21().Run();
